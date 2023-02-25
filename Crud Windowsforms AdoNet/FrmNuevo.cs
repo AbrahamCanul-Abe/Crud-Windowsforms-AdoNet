3478993f58073852443dc3d32bd476597474b764
Abraham Canul <abraham23.acc@gmail.com>
2023-02-25 14:09:42 -0600

    formulario agregar productos

---
+++ Crud Windowsforms AdoNet/FrmNuevo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crud_Windowsforms_AdoNet
+{
+    public partial class FrmNuevo : Form
+    {
+        public FrmNuevo()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
