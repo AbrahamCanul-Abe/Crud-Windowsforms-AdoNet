8ef2f865f01fb9218bfd192b7e7d602d5b0578be
Abraham Canul <abraham23.acc@gmail.com>
2023-03-01 13:35:33 -0600

    field names y props de Productos

---
+++ ControlVentasCore/Entity/ProductoInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlVentasCore.Entity
+{
+    /// <summary>
+    /// Definicion de clase que permite exponer los atributos de un producto
+    /// </summary>
+    public class ProductoInfo
+    {
+        #region Database FieldNames
+        public class FieldName
+        {
+            public const string Id = "Id";
+            public const string Nombre = "Nombre";
+            public const string Descripcion = "Descripcion";
+            public const string Precio = "Precio";
+            public const string CategoriaId = "CategoriaId";
+        }
+        #endregion
+        #region Properties
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public int CategoriaId { get; set; }
+        #endregion
+    }
+}
