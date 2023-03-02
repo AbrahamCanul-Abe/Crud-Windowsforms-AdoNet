e263a4af5e229f3a709698a1b3b00028b06a15e5
Abraham Canul <abraham23.acc@gmail.com>
2023-03-02 14:20:50 -0600

    capa de negocios

---
+++ ControlVentasCore/Business/ProductoBAL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlVentasCore.Business
+{
+    /// <summary>
+    /// Definicion de clase que permite exponer metodos de negocio para los productos
+    /// </summary>
+    public class ProductoBAL
+    {
+        #region Variables Globales...
+        private Data.ProductoDAL ProductoDAL;
+        #endregion
+
+        #region Constructor...
+        public ProductoBAL()
+        {
+            ProductoBAL = new Data.ProductoDAL();
+        }
+        #endregion
+    }
+}
