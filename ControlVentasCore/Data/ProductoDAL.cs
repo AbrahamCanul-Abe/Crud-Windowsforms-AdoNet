28d16d0880c4f13c0cc1b308981458d399f81b0d
Abraham Canul <abraham23.acc@gmail.com>
2023-03-02 11:40:38 -0600

    metodos insert, update y delete

--- ControlVentasCore/Data/ProductoDAL.cs
+++ ControlVentasCore/Data/ProductoDAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ControlVentasCore.Entity;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -9,11 +10,16 @@ namespace ControlVentasCore.Data
 {
     public class ProductoDAL
     {
+        #region Global Variables
+        public const string TableName = "Productos";
+        #endregion
+
         #region Properties
         /// <summary>
         /// String de conexion a la bd
         /// </summary>
         public string ConnectionString { get; set; }
+
         #endregion
 
         #region Methods 
@@ -25,7 +31,12 @@ namespace ControlVentasCore.Data
         /// <returns></returns>
         public Entity.ProductoInfo GetEntityObject(int Id)
         {
-            
+            string SQL = $"select * from {TableName} where {Entity.ProductoInfo.FieldName.Id} = {Id}";
+            DataTable dt = Utilerias.SQLHelper.ExecuteDatatable(SQL, ConnectionString);
+
+            if (dt == null || dt.Rows.Count == 0) throw new Exception($"No pude obtener los datos del Id [{Id}]");
+
+            return GetEntityObject(dt.Rows[0]);
         }
 
         /// <summary>
@@ -33,7 +44,7 @@ namespace ControlVentasCore.Data
         /// </summary>
         /// <param name="Row"></param>
         /// <returns></returns>
-        private Entity.ProductoInfo GetEntityObject(DataRow Row)
+        public Entity.ProductoInfo GetEntityObject(DataRow Row)
         {
             if (Row == null) throw new ArgumentNullException("No recibimos el datarow para obtener los datos");
 
@@ -42,7 +53,7 @@ namespace ControlVentasCore.Data
             ProductoInfo.Nombre = Row[Entity.ProductoInfo.FieldName.Nombre].ToString();
             ProductoInfo.Descripcion = Row[Entity.ProductoInfo.FieldName.Descripcion].ToString();
             ProductoInfo.Precio = Convert.ToDecimal(Row[Entity.ProductoInfo.FieldName.Precio].ToString());
-            ProductoInfo.CategoriaId = Convert.ToInt32([Entity.ProductoInfo.FieldName.CategoriaId]);
+            ProductoInfo.CategoriaId = Convert.ToInt32(Row[Entity.ProductoInfo.FieldName.CategoriaId]);
 
             return ProductoInfo;
         }
@@ -54,7 +65,24 @@ namespace ControlVentasCore.Data
         /// <returns></returns>
         public int Insert(Entity.ProductoInfo ProductoInfo)
         {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"insert into {TableName} (");
+            sb.Append(Entity.ProductoInfo.FieldName.Nombre);
+            sb.Append($", {Entity.ProductoInfo.FieldName.Descripcion}");
+            sb.Append($", {Entity.ProductoInfo.FieldName.Precio}");
+            sb.Append($", {Entity.ProductoInfo.FieldName.CategoriaId}");
+            sb.AppendLine(")");
+            sb.AppendLine("values(");
+            sb.Append($"'{ProductoInfo.Nombre}'");
+            sb.Append($",'{ProductoInfo.Descripcion}'");
+            sb.Append($",'{ProductoInfo.Precio}'");
+            sb.Append($",'{ProductoInfo.CategoriaId}'");
+            sb.AppendLine(")");
+            sb.AppendLine("select SCOPE_IDENTITY");
 
+            object Id = Utilerias.SQLHelper.ExecuteScalar(sb.ToString(), ConnectionString);
+            if (Id == null) return 0;
+            return Convert.ToInt32(Id);
         }
 
         /// <summary>
@@ -62,9 +90,18 @@ namespace ControlVentasCore.Data
         /// </summary>
         /// <param name="ProductosInfo"></param>
         /// <returns></returns>
-        public int Update(Entity.ProductoInfo ProductosInfo)
+        public int Update(Entity.ProductoInfo ProductoInfo)
         {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"update {TableName} set");
+            sb.Append($"{Entity.ProductoInfo.FieldName.Nombre} = '{ProductoInfo.Nombre}'");
+            sb.Append($",{Entity.ProductoInfo.FieldName.Descripcion} = '{ProductoInfo.Descripcion}'");
+            sb.Append($"{Entity.ProductoInfo.FieldName.Precio} = '{ProductoInfo.Precio}'");
+            sb.Append($"{Entity.ProductoInfo.FieldName.CategoriaId} = '{ProductoInfo.CategoriaId}'");
+            sb.Append($" where {Entity.ProductoInfo.FieldName.Id} = {ProductoInfo.Id}");
 
+            Utilerias.SQLHelper.ExecuteNonQuery(sb.ToString(), ConnectionString);
+            return ProductoInfo.Id;
         }
 
         /// <summary>
@@ -74,7 +111,9 @@ namespace ControlVentasCore.Data
         /// <returns></returns>
         public int Delete(int Id)
         {
-
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"delete {TableName}");
+            sb.Append($" where {Entity.ProductoInfo.FieldName.Id} = {Id}");
         }
 
         /// <summary>
